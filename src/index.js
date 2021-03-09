94ee66026be3d908f0bbc5b92f097a0f2131274b
Antonina Vasilieva <tonya_verkooijen@mail.ru>
2021-03-09 20:42:00 +0300

    feat: make it work correctly after npm test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  bla bla
+  let arrBrackets = [],
+  openedBrackets = '({[',
+  closedBrackets = ')}]';
+
+  for(let part of str) {
+        let i = openedBrackets.indexOf(part);
+        if (i > -1) {
+            arrBrackets.push(closedBrackets[i]);
+        }
+        if (closedBrackets.includes(part) && part!= arrBrackets.pop()) {
+            return false;
+        }
+  }
+  return arrBrackets === 0;
+
 }
