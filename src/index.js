a3652fe4de4c1ee2a8e784f947994d73d40dfb20
Antonina Vasilieva <tonya_verkooijen@mail.ru>
2021-03-09 22:55:02 +0300

    feat: add new brackets

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  let arrBrackets = [],
-  openedBrackets = '({[',
-  closedBrackets = ')}]';
 
-  for(let part of str) {
-        let i = openedBrackets.indexOf(part);
-        if (i > -1) {
-            arrBrackets.push(closedBrackets[i]);
-        }
-        if (closedBrackets.includes(part) && part!= arrBrackets.pop()) {
-            return false;
-        }
-  }
-  return arrBrackets === 0;
+    let count = 0;
 
+    for (let i = 0; i < str.length; i ++) {
+
+        if (str[i] == '(') count++; else if (str[i] == ')') count--;
+        if (str[i] == '{') count++; else if (str[i] == '}') count--;
+        if (str[i] == '[') count++; else if (str[i] == ']') count--;
+        if (count < 0) break;
+    }
+
+    if (count != 0) return false;
+    else return true;
 }
