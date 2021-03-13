9a38139a5ce9bbe926576ab2557802839fd43e34
Antonina Vasilieva <tonya_verkooijen@mail.ru>
2021-03-13 14:15:44 +0300

    feat: check the match of parentheses

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const { compareLoose } = require("semver");
+
 module.exports = function check(str, bracketsConfig) {
     
     let stack = [], 
@@ -9,6 +11,18 @@ module.exports = function check(str, bracketsConfig) {
 
     for (let i = 0, fullLength = arr.length; i < fullLength; i ++) {
         openIndex = openedBrackets.indexOf(arr[i]);
+        if (openIndex !== -1) {
+            stack.push(openIndex);
+            continue;
+        }
 
+        closeIndex = closedBrackets.indexOf(arr[i]);
+        if (closeIndex !== -1) {
+            openIndex = stack.pop();
+            if (closeIndex !== openIndex) {
+                return false;
+            }
+        }
     }
+
 }
