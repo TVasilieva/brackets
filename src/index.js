423c6cdda417e720420d9ed1c49e17184c2b6165
Antonina Vasilieva <tonya_verkooijen@mail.ru>
2021-03-14 18:40:15 +0300

    feat: complete the task, but will change it in some days for a better version

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
-const { compareLoose } = require("semver");
-
 module.exports = function check(str, bracketsConfig) {
     
-    let stack = [], 
-    openedBrackets = ['(','[','{'], 
-    closedBrackets = [')','}',']'],
-    arr = str.split('')
-    closeIndex,
-    openIndex;
+    let brackets = bracketsConfig.map((bracket) => bracket.join('')); 
 
-    for (let i = 0, fullLength = arr.length; i < fullLength; i ++) {
-        openIndex = openedBrackets.indexOf(arr[i]);
-        if (openIndex !== -1) {
-            stack.push(openIndex);
-            continue;
-        }
-
-        closeIndex = closedBrackets.indexOf(arr[i]);
-        if (closeIndex !== -1) {
-            openIndex = stack.pop();
-            if (closeIndex !== openIndex) {
-                return false;
-            }
-        }
+    for (let i = 0; i < brackets.length; i++) {
+  
+      if (str.includes(brackets[i])) {
+        str = str.replace(brackets[i], '');
+        i = -1;
+      }
     }
-    if (stack.length !== 0) {
-        return false
-    }
-    
-    return true;
-}
+  
+    return !str;
+  }
